50996cb9c575e4b874c4c57eb0ba273c070c9259
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-09-24 19:17:03 +0530

    made changes

--- backend/server.js
+++ backend/server.js
@@ -56,12 +56,34 @@ function broadcaseRoomUserlist(room) {
     io.to(room).emit('room:userlist',{room,users});
 }
 
+//This is what runs before the server accepts connection from a socket.
+//The the following function sees if the handshake from 'client' has {auth:[<token>]}.
 io.use((socket,next)=>{
+    //See if the 'auth' object exists and 'auth.token' values exists:
     const token = socket.handshake.auth && socket.handshake.auth.token;
+    //If no token is found send error:
     if (!token) return next(new Error('Auth error : token required'));
+
+    //Try to verify the JWT:
     try {
         const payload = jwt.verify(token,process.env.SEC)
+        //Attach the 'user' object to the socket.
+        socket.user   = payload
+        //Go the the next middleware ie., the 'io.on(connection,.....)' shit you see below after this function
+        return next();
+    } catch (err) {
+        console.error('socket auth fail: ',err.message);
+        return next(new Error('Authentication error : invalid token'));
     }
+});
+
+io.on('connection',(socket)=>{
+    //Console log that socket has connected and log his/her id and username:
+    console.log('Socket connected',socket.id,'user: ',socket.user.username);
+    //Notify Everyone Connected:
+    socket.broadcast.emit('user:joined',{username:socket.user.username,id:socket.id});
+    //Listen for chat messages sent by any connected socket/ user:
+    
 })
 
 
