cea8f292c5c3f662cab712ab2ee1d8976a20583e
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-09-24 16:41:51 +0530

    made changes

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,11 @@ const app      = express();
 const server   = http.createServer(app);
 const {Server} = require('socket.io');
 
+const io       = new Server(server,{
+    cors:{origin:'*'}
+})
+
+//middlewares:
+app.use(cors()); //Used to escape cors errors
+app.use(express.json()); //Enables parsing of incoming request bodies in JSON format.
+
